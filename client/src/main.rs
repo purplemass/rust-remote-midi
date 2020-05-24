4f2c6769b6562a40c54eae07df255d665cbff72a
Bob Hatamian <bob@purplemass.com>
2020-05-25 00:52:52 +0100

    Add midi module

--- client/src/main.rs
+++ client/src/main.rs
@@ -6,7 +6,6 @@ use std::env;
 use std::io::{self, ErrorKind, Read, Write};
 use std::net::TcpStream;
 use std::process;
-use std::sync::{Arc, Mutex};
 use std::sync::mpsc::{self, TryRecvError};
 use std::thread;
 use std::time::Duration;
@@ -15,8 +14,7 @@ use chrono::prelude::*;
 use rand::{thread_rng, Rng};
 use uuid::Uuid;
 
-use midir::{MidiOutput};
-use midir::os::unix::{VirtualOutput};
+mod midi;
 
 const SERVER_PORT: &str = "6000";
 const MIDI_OUTPORT_ID: &str = "REMOTE_MIDI";
@@ -32,9 +30,7 @@ fn main() {
 
     print_welcome(uuid, &server_address, &midi_port);
 
-    let midi_out = MidiOutput::new("RemoteMidiOutput").unwrap();
-    let conn_out = midi_out.create_virtual(midi_port).unwrap();
-    let conn_out_shared = Arc::new(Mutex::new(conn_out));
+    let conn_out_shared = midi::create_port(midi_port);
 
     let tx = check_stream(uuid, &server_address, conn_out_shared);
 
@@ -87,13 +83,13 @@ fn check_stream(uuid: Uuid, server_address: &String, conn_out: std::sync::Arc<st
                     print_log(&format!("< {}", get_msg(&msg)).to_string());
                     let mut rng = thread_rng();
                     match msg_vec[1] {
-                        "a" => play_note(conn_out.clone(), 12, 1),
-                        "b" => play_note(conn_out.clone(), 15, 1),
-                        "1" => play_single_note(conn_out.clone(), 0x9E, 12, 127),
-                        "2" => play_single_note(conn_out.clone(), 0x8E, 12, 0),
-                        "3" => play_single_note(conn_out.clone(), 0x9E, 15, 127),
-                        "4" => play_single_note(conn_out.clone(), 0x8E, 15, 0),
-                        _ => play_note(conn_out.clone(), rng.gen_range(50, 80), 1),
+                        "a" => midi::play_note(conn_out.clone(), 12, 1),
+                        "b" => midi::play_note(conn_out.clone(), 15, 1),
+                        "1" => midi::play_single_note(conn_out.clone(), 0x9E, 12, 127),
+                        "2" => midi::play_single_note(conn_out.clone(), 0x8E, 12, 0),
+                        "3" => midi::play_single_note(conn_out.clone(), 0x9E, 15, 127),
+                        "4" => midi::play_single_note(conn_out.clone(), 0x8E, 15, 0),
+                        _ => midi::play_note(conn_out.clone(), rng.gen_range(50, 80), 1),
                     }
                 }
             },
@@ -120,26 +116,6 @@ fn check_stream(uuid: Uuid, server_address: &String, conn_out: std::sync::Arc<st
     });
 
     tx
-}
-
-fn play_single_note(conn_out: std::sync::Arc<std::sync::Mutex<midir::MidiOutputConnection>>, note_msg: u8, note: u8, velocity: u8) {
-    let mut conn_out_shared = conn_out.lock().unwrap();
-    let _ = conn_out_shared.send(&[note_msg, note, velocity]);
-}
-
-fn play_note(conn_out: std::sync::Arc<std::sync::Mutex<midir::MidiOutputConnection>>, note: u8, duration: u64) {
-    // https://people.carleton.edu/~jellinge/m208w14/pdf/02MIDIBasics_doc.pdf
-    // channel 1:  0x90 off 0x80 on
-    // channel 16: 0x9F off 0x8F on
-    const NOTE_ON_MSG: u8 = 0x9E;
-    const NOTE_OFF_MSG: u8 = 0x8E;
-    // const VELOCITY: u8 = 0x64;
-    // We're ignoring errors in here
-    let mut conn_out_shared = conn_out.lock().unwrap();
-    let _ = conn_out_shared.send(&[NOTE_ON_MSG, note, 127]);
-    thread::sleep(Duration::from_millis(duration * 150));
-    let _ = conn_out_shared.send(&[NOTE_OFF_MSG, note, 0]);
-    // print_log(&format!("play note {}", note).to_string());
 }
 
 fn get_time() -> chrono::DateTime<chrono::Utc> {
---
+++ client/src/midi.rs
@@ -0,0 +1,35 @@
+extern crate midir;
+
+use std::sync::{Arc, Mutex};
+use std::thread;
+use std::time::Duration;
+
+use midir::{MidiOutput, MidiOutputConnection};
+use midir::os::unix::{VirtualOutput};
+
+
+pub fn create_port(midi_port: &str) -> Arc<Mutex<MidiOutputConnection>> {
+    let midi_out = MidiOutput::new("RemoteMidiOutput").unwrap();
+    let conn_out = midi_out.create_virtual(midi_port).unwrap();
+    Arc::new(Mutex::new(conn_out))
+}
+
+pub fn play_single_note(conn_out: Arc<Mutex<MidiOutputConnection>>, note_msg: u8, note: u8, velocity: u8) {
+    let mut conn_out_shared = conn_out.lock().unwrap();
+    let _ = conn_out_shared.send(&[note_msg, note, velocity]);
+}
+
+pub fn play_note(conn_out: Arc<Mutex<MidiOutputConnection>>, note: u8, duration: u64) {
+    // https://people.carleton.edu/~jellinge/m208w14/pdf/02MIDIBasics_doc.pdf
+    // channel 1:  0x90 off 0x80 on
+    // channel 16: 0x9F off 0x8F on
+    const NOTE_ON_MSG: u8 = 0x9E;
+    const NOTE_OFF_MSG: u8 = 0x8E;
+    // const VELOCITY: u8 = 0x64;
+    // We're ignoring errors in here
+    let mut conn_out_shared = conn_out.lock().unwrap();
+    let _ = conn_out_shared.send(&[NOTE_ON_MSG, note, 127]);
+    thread::sleep(Duration::from_millis(duration * 150));
+    let _ = conn_out_shared.send(&[NOTE_OFF_MSG, note, 0]);
+    // print_log(&format!("play note {}", note).to_string());
+}
